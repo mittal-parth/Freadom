f4142224c5885f7179313eabf63480ef98a51642
Parth <mittalparth22@gmail.com>
2021-11-16 22:00:05 +0530

    adding index page

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        
+    return render(request, 'index.html')
