751ca85c991eb968081efb7d9f32867175212841
Parth <mittalparth22@gmail.com>
2021-11-16 22:45:10 +0530

    connecting scraping script

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+import requests, sys
+from subprocess import run, PIPE
 
 # Create your views here.
 def index(request):
+    context = {'output':''}
     if request.method == 'POST':
         url = request.POST.get('url')
-        
-    return render(request, 'index.html')
+        output = run([sys.executable,'D:\\PARTH DATA\\WEB DEVELOPMENT\\WEC Recruitment Tasks\\web_scraper\\script.py',url], shell=False, stdout=PIPE)
+        context = {'output':output.stdout}
+    return render(request, 'index.html', context)
---
+++ script.py
@@ -0,0 +1,38 @@
+import docx
+import re
+import requests
+import sys
+from bs4 import BeautifulSoup
+
+mydoc = docx.Document()
+
+def getInfo(url):
+
+    # url = 'https://medium.com/publishous/a-piece-of-advice-i-desperately-want-every-creative-to-hear-fdb878d87b6e'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    post = soup.find('article')
+
+    data = post.text
+    print(data)
+    mydoc.add_paragraph(data)
+
+    # Change file name and path here
+    mydoc.save("D:\\PARTH DATA\\WEB DEVELOPMENT\\WEC Recruitment Tasks\\web_scraper_result.docx")
+
+def main():
+    try:
+        url = sys.argv[1]
+        # print(f"Getting Page from {url}")
+        getInfo(url)
+    except Exception as e:
+        print(f"Got Exception on Page as {e}")
+        return
+
+    # print('Done')
+
+# Driver Code
+main()
